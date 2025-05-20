8384400f3a45f294a8a3798eeff29d2507c2afea
nullxplorer <nullxplorer@gmail.com>
2025-05-20 19:28:27 +0530

    update hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,29 @@
+require("@matterlabs/hardhat-zksync");
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.20",
+
+  zksolc: {
+    version: "1.5.12", // Specify version instead of "latest"
+    settings: {
+      codegen: "evmla" // Add this to use EVM legacy assembly codegen
+    }
+  },
+  defaultNetwork: "lensTestnet",
+
+  networks: {
+    lensTestnet: {
+      chainId: 37111,
+      ethNetwork: "sepolia",
+      url: process.env.RPC_URL || "https://rpc.testnet.lens.xyz",
+      verifyURL: "https://block-explorer-verify.testnet.lens.dev/contract_verification",
+      zksync: true,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    hardhat: {
+      zksync: true
+    }
+  }
+};
